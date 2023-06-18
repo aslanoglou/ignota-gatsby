92bd4f882a7fbc6c29dbad17c8d559a8aa183c79
Jordan <i.aslanoglou@gazzetta.gr>
2023-06-19 00:03:11 +0300

    transform animation / services clientportfolio

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,7 +7,7 @@ const Services = () =>{
     const { ref, inView } = useInView({triggerOnce: true});
     return(
         <section ref={ref}>
-            <div className="md:mb-[256px] mb-[181px] cursor-default data-[inviewport=false]:invisible data-[inviewport=false]:opacity-0 data-[inviewport=false]:tranform data-[inviewport=false]:translate-y-14 transition ease-in-out duration-300 delay-100" data-inviewport={inView.toString()}>
+            <div className="md:mb-[256px] mb-[181px] cursor-default data-[inviewport=false]:invisible data-[inviewport=false]:opacity-0 data-[inviewport=false]:tranform data-[inviewport=false]:translate-y-20 transition ease-in-out duration-300 delay-100" data-inviewport={inView.toString()}>
                 <h2 className="text-lg md:text-[40px] leading-sm relative pt-12 mb-[50px] font-NeueMachina text-ignota-white before:content-[''] before:block before:top-0 before:absolute data-[inviewport=false]:before:w-0 before:border-t before:border-t-ignota-white data-[inviewport=true]:before:w-[80px] transition before:ease-in-out before:duration-300 before:delay-700"
                     data-inviewport={inView.toString()} >
                     Our services
